129259cb4d3e39c3cf788a3616b4e7888053e40d
akkivishnoi3333-ctrl <akkivishnoi3333@gmail.com>
2026-04-24 23:36:25 +0530

    Update app.js

--- hotel-management-backend/app.js
+++ hotel-management-backend/app.js
@@ -33,14 +33,13 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-const path = require("path");
 
-// Serve frontend build
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../hotel-management-frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../hotel-management-frontend/build", "index.html"));
 });
+
 
   // render the error page
   res.status(err.status || 500);
